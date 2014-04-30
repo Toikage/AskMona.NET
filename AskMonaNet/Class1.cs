532a0f436db6583b03b29c0a9e0d848549aac0ab
AtKey <at.short.key@outlook.com>
2014-05-01 00:07:25 +0900

    First

---
+++ AskMonaNet/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace AskMonaNet
+{
+    public static partial class AskMonaNet
+    {
+		JObject GetTopicList(TopicOrder order,int limit,int offset)
+		{
+			return Call("", string.Format("?order={0}", order.ToString(), limit, offset));
+			//JsonConvert.
+		}
+
+		
+    }
+
+	/// <summary>
+	/// 並べ替える条件。'updated'は更新順、'created'は作成順、'favorites'はお気に入り順、'recieve'はやり取りされたMONA順で並べ替えられます。
+	/// </summary>
+	public enum TopicOrder
+	{
+		updated,created,favorites,recieve
+	}
+
+}
---
+++ AskMonaNet/Topic.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AskMonaNet
+{
+
+	public class Topic
+	{
+
+	}
+	public class TopicRes
+	{
+
+	}
+
+	public struct TopicId : Int32
+	{
+
+	}
+
+}
